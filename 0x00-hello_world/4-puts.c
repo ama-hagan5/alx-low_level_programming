3491020666a939ad5182baf372691560a881b4de
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-07 08:39:59 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle"):
-		reeturn (0):
+	puts("\" Programming is like building a multilingual puzzel");
+	return (0):
 }
