27b921f6ecf2cacd3d4348851112931905393deb
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-08 13:42:43 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success/correct)
  */
@@ -20,9 +20,9 @@ int main(void)
 		putchar(upper_case);
 	}
 
-	
 	putchar ('\n');
 
 	return (0);
 
 }
+
