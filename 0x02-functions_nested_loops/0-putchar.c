5238d5713c563704e067799c9fe2d7126eebda81
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-12 08:32:24 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main-main block
- * Description:print -putchar in a new line
- * Return:this program returns zero
+ * main - Entry point
+ *
+ * Return: nothing, return void
  */
+
 int main(void)
 {
 	_putchar('_');
@@ -15,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
