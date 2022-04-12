35dd12dce52c86cd23add0565748e06872a5a47d
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-12 15:56:05 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,10 +6,12 @@
  */
 void times_table(void)
 {
-	int a, b, c;
-	for (a = 0; a <= 9; a++)
+	int a;
+	int b;
+	int c;
+	for (a =0; a<= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (b =0; b <= 9; b++)
 		{
 			c = a * b;
 			if ((c / 10) == 0)
@@ -20,8 +22,8 @@ void times_table(void)
 				}
 				if (b != 0)
 				{
-					_putchar (' ');
-					_putchar ((c % 10) +'0');
+					_putchat (' ');
+					_putchar (( c % 10) + '0');
 				}
 				if (b < 9)
 				{
@@ -32,7 +34,7 @@ void times_table(void)
 			else
 			{
 				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) +'0');
+				_putchar ((c % 10) + '0');
 				if (b < 9)
 				{
 					_putchar(',');
