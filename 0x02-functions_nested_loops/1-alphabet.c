dcdd46bc14174955dbd4ca696331f763389b887e
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-12 10:40:25 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#includen"main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		_putchar(alphabet);
+	}
+
+	putchar('/n');
+
+	return (0);
+
+}
