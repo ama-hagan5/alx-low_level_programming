d9c313c55cf4014cca77b9384be5abefa6ad943a
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-14 14:02:09 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,26 +2,24 @@
 /**
  * print_line - draws a straight line in the terminal.
  * @n: print int
+ *
  * Return: Always 0.
  */
 void print_line(int n)
 {
+	int x;
 
-	int i;
-
-	if (n > 0)
+	if (n <= 0)
 	{
-		for (i = 0; i < n; i++)
-		{
-			_putchar (95);
-		}
-
-		_putchar ('\n');
+		_putchar('\n');
 	}
-
 	else
 	{
-		_putchar ('\n');
-	}
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
 
+		_putchar('\n');
+	}
 }
