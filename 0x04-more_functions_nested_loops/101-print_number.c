b7c5cf7c4a894313a48bd9422768799a3e276aaf
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-14 15:44:35 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,19 @@
  */
 void print_number(int n)
 {
-	unsigned int k = n;
+	unsigned int m;
 
 	if (n < 0)
 	{
-		n *= -1;
-		k = n;
 		_putchar('-');
+		m = n;
 	}
-
-	k /= 10;
-
-	if (k != 0)
-		print_number(k);
-
-	_putchar((unsigned int) n % 10 + '0');
-
+	else
+	{
+		m = n;
+	}
+	if (m / 10 != 0)
+		printf_number(m / 10);
+	_putchar((m % 10) + '0');
 }
+
