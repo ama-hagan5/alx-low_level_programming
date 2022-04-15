a3838bde4ae5f097458a5d1490628f6bf1484dc5
ama-hagan5 <101412951+ama-hagan5@users.noreply.github.com>
2022-04-15 22:26:47 +0000

    Task 1

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,19 @@
 #include "main.h"
 /**
- * _isupper - evalue if letter is uppercase
- * @c: print int
+ * _isdigit - if is a digit
+ *@c: print int
  * Return: Always 0.
  */
 
-int _isupper(int c)
+int _isdigit(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-
-	else
-	{
-		return (0);
-	}
-
+if (c >= 48 && c <= 57)
+{
+return (1);
 }
 
+else
+{
+return (0);
+}
+}
