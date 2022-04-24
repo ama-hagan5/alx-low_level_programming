8d5d49ee67a72aca81eccd1160b2970c2db335a1
ama-hagan5 <101412951+ama-hagan5@users.noreply.github.com>
2022-04-24 17:38:35 +0000

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * infinite_add - adds two numbers
- * @1: number one.
+ * @n1: number one.
  * @n2: number two.
  * @r: buffer that the function will use to store the result.
  * @size_r: buffer size:
@@ -11,7 +11,7 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int c1 = 0, c2 = 0, op, bg, drl, dr2, add = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
 	while (*(n1 + c1) != '\0')
 		c1++;
@@ -23,7 +23,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		bg = c2;
 	if (size_r <= bg + 1)
 		return (0);
-	r[bg +1] = '\0';
+	r[bg + 1] = '\0';
 	c1--, c2--, size_r--;
 	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
 	while (bg >= 0)
@@ -34,7 +34,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		else
 			add = 0;
 		if (op > 0)
-			*(r + bg) =(op % 10) + 48
+		*(r + bg) = (op % 10) + 48;
 		else
 			*(r + bg) = '0';
 		if (c1 > 0)
