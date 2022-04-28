dc4522be44b9eee40149132b7799dbda71f2663b
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-04-28 13:57:21 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,13 +3,13 @@
 /**
  * wildcmp - compares strings
  * @s1: pointer to string parameters
- * s2: pointer to string parameters
+ * @s2: pointer to string parameters
  * Return: 0
  */
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 =='\0')
+	if (*s1 == '\0')
 	{
 		if (*s2 != '\0' && *s2 == '*')
 		{
@@ -24,7 +24,7 @@ int wildcmp(char *s1, char *s2)
 	}
 	else if (*s1 == *s2)
 	{
-		return (wildcmp(s1 + 1, s2 +1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
 }
