c1382a78dfeb481e6ae52484aa19cac2e90cdf38
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-05-04 16:22:06 -0700

    Task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - print the name of the program 
+ * main - print the name of the program
  * @argc: Count arguments
  * @argv: Arguments
  *
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
-	printf("%i\n", argc -1);
+	printf("%i\n", argc  1);
 
 	return (0);
 }
