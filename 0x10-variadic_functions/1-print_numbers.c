52fa161ac7d33c686465cf0de40336c49a6502c8
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-05-17 07:36:08 -0700

    Task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,17 +7,17 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list list;
+	va_list valist;
 	unsigned int i;
 
-	va_start(list, n)
+	va_start(valist, n)
 
 		for (i = 0; i < n; i++)
 		{
-			printf("%d", va_arg(list, int));
+			printf("%d", va_arg(valist, int));
 			if (separator && i < n - 1)
 				printf("%s", separator);
 		}
 	printf("\n");
-	va_end(list);
+	va_end(valist);
 }
