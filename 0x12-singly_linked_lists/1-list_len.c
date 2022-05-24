d8099326386b52d9adfbd1e282401c6251c5285b
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-05-24 14:43:44 -0700

    Singly Task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
-/**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
 
+/**
+ * list_len - number of elements in a linked list_t list.
+ * @h: pointer
+ * Return: number of nodes in the list
+ */
 size_t list_len(const list_t *h)
 {
-    size_t nelem;
+	size_t len = 0;
+	int i;
+	const list_t *c = h;
 
-   nelem = 0;
-   while (h != NULL)
-   {
-        h = h->next;
-        nelem++;
-   }
-   return (nelem);
+	for (i = 0; c; i++)
+	{
+	c = c->next;
+	len++;
+	}
+return (len);
 }
