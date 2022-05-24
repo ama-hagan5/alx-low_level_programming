b679c14f5754df3ab2c80f7e1778632a1cc2c77d
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-05-24 15:01:24 -0700

    Task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,19 @@
 #include "lists.h"
 
 /**
- * free_list - frees a list
- * @head: head of the linked list.
- * Return: no return.
+ * free_list - Frees a list_t
+ * @head: A pointer to the list_t
+ * Return:void
  */
-
 void free_list(list_t *head)
 {
-    list_t *current;
+	list_t *check, *c;
 
-    while ((current = head) != NULL)
-    {
-        head = head->next;
-        free(current->str);
-        free(current);
-    }
+c = head;
+while (c != NULL)
+{
+	check = c->next;
+	free(c->str);
+	free(c);
+	c = check;
 }
