6cbdec80f42db5bc100790ccbc81f57f8b1468d8
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-05-30 16:08:53 -0700

    Tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,27 +8,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-        int fd, len, w;
+	int fd, len, w;
 
-        if (filename == NULL)
-                return (-1);
+	if (filename == NULL)
+		return (-1);
 
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
-        if (fd == -1)
-        {
-                return (-1);
-        }
-        if (text_content != NULL)
-        {
-                for (len = 0; text_content[len] != '\0'; len++)
-                ;
-                w = write(fd, text_content, len);
-        }
-        if (w == -1)
-        {
-                return (-1);
-        }
-        close(fd);
-        return (1);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+		;
+		w = write(fd, text_content, len);
+	}
+	if (w == -1)
+	{
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
-
