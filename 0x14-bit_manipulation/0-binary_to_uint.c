bda9bf2b5e956f8644bafee0cc4db3ce9181b1be
Dorcas Henrietta Larsey <henriettalarsey@gmail.com>
2022-06-02 11:39:11 -0700

    Bit task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,25 @@
 #include "main.h"
-
 /**
- * _strlen - lenght of string
- * @s:char
- * Return:int
- */
-int _strlen(const char *s)
-{
-	int i;
-
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			continue;
-		}
-return (i);
-}
-/**
- * binary_to_uint - convert binary to unsigned int
- * @b:string from 0 and 1 else NULL
+ * binary_to_uint - binary to uint
+ * @b: string to convert
+ * Return: unsigned int
  *
- * Return:unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int k = 1;
-	unsigned int i = 0;
-	int c;
-	unsigned int len;
+	unsigned int x = 0;
 
 	if (b == NULL)
 		return (0);
-
-	len = _strlen(b);
-
-	for (c = len - 1; c >= 0; c--)
+	while (*b)
 	{
-	if (b[c] != '0' && b[c] != '1')
-		return (0);
-	if (b[c] == '1')
-	{
-		i += k;
+		if (*b == '1')
+			x = (x << 1) | 1;
+		else if (*b == '0')
+			x <<= 1;
+		else
+			return (0);
+		b++;
 	}
-	k *= 2;
-}
-	return (i);
+	return (x);
 }
