1cdb34bc27ca2a9be24527f5b51ee3a248659cd1
ama-hagan5 <101412951+ama-hagan5@users.noreply.github.com>
2022-06-02 20:05:42 +0000

    Bit task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,34 +1,25 @@
 #include "main.h"
 
 /**
- * binary_to_uint- convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0, j;
-	unsigned int number = 0, non_binary = 9;
+    int i;
+    unsigned int dec_val = 0;
 
-	if (b == NULL)
-		return (non_binary);
+    if (!b)
+        return (0);
 
-	while (b[len] != '\0')
-		len++;
-	len -= 1;
+    for (i = 0; b[i]; i++)
+    {
+        if (b[i] < '0' || b[i] > '1')
+            return (0);
+        dec_val = 2 * dec_val + (b[i] - '0');
+    }
 
-
-	j = 0;
-	while (b[j])
-	{
-		if ((b[j] != '0') && (b[j] != '1'))
-			return (non_binary);
-
-		if (b[j] == '1')
-			number += (1 * (1 << len));
-		j++;
-		len--;
-	}
-
-	return (number);
+    return (dec_val);
 }
